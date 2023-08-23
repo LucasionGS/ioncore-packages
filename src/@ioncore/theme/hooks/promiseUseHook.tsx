9643e4e6946d6dc82762f749e29006c17ecd9f18
Lucasion <lucasion@hotmail.com>
2023-08-23 03:21:11 +0200

    Added promiseUseHook and SelectInput

---
+++ src/@ioncore/theme/hooks/promiseUseHook.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+/**
+ * Converts a promise to a hook.
+ * @returns A hook function that returns a tuple of the promise result and a refresh function. Defaults to null until the promise is resolved.
+ * @example
+ * const fetchUser = (userId) => fetch(`/api/user/${userId}`).then(res => res.json());
+ * const useUser = promiseUseHook(fetchUser); // useUser is now a hook and can be used in a component
+ * 
+ * const MyComponent = () => {
+ *   const [user, refreshUser] = useUser(123); // user is null until the promise is resolved and UI is updated.
+ *   return <div>
+ *     <p>User is {user?.name}</p>
+ *     <button onClick={refreshUser}>Refresh</button>
+ *   </div>
+ * }
+ */
+export default function promiseUseHook<T, const TArgs extends any[]>(fn: (...args: TArgs) => PromiseLike<T>) {
+  return function useHook(...args: TArgs) {
+    const [state, setState] = React.useState<T | null>(null);
+    const refresh = (...args: TArgs) => {
+      fn(...args).then(setState);
+    }
+    React.useEffect(() => {
+      refresh(...args);
+    }, []);
+    return [state, refresh] as const;
+  }
+}
